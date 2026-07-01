9e0c74b63a5961c8e893b7fe70d429174fa8b1a7
Gojodev <Jirayasarutobi35@gmail.com>
2026-07-01 15:21:23 +0100

    experience section done

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import Navbar from '@/components/layout/Navbar'
 import Hero from '@/components/sections/Hero'
 import About from '@/components/sections/About'
 import Skills from '@/components/sections/Skills'
+import Experience from '@/components/sections/Experience'
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <About />
       <Skills />
+      <Experience />
     </>
   )
 }
